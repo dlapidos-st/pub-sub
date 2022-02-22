e89667bb3f3c75ac884479011edd387c1534af90
Dan Lapidos <dlapidos@servicetitan.com>
2022-02-22 15:21:15 -0700

    Read from ACP topic

---
+++ TenantConfigurationMessageHandler.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json;
+
+using ServiceTitan.Messaging.CapacityPlanning.Protos;
+using ServiceTitan.Platform.PubSub;
+
+namespace PubSub
+{
+    public class TenantConfigurationMessageHandler : IMessageHandler<TenantConfiguration>
+    {
+        private readonly JsonSerializerOptions jsonOptions = new() { PropertyNamingPolicy = JsonNamingPolicy.CamelCase, WriteIndented = true };
+
+        public ValueTask HandleAsync(TenantConfiguration message, IConsumeContext context)
+        {
+            Console.WriteLine(JsonSerializer.Serialize(message, this.jsonOptions));
+            return ValueTask.CompletedTask;
+        }
+    }
+}
